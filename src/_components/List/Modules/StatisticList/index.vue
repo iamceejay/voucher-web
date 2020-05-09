a81633634b625d2f7198836e5c205589787047f9
naio.edio <karlnaio.edio@fligno.com>
2020-05-09 14:39:11 +0800

    Statistics change one col to two col

--- src/_components/List/Modules/StatisticList/index.vue
+++ src/_components/List/Modules/StatisticList/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="flex flex-wrap">
+  <div class="flex flex-wrap ">
     <div
       v-for="(row, index) in data"
       :key="`stat-${index}`"
-      class="w-full sm:w-1/2 md:w-1/4	lg:w-1/5 xl:w-1/6 border rounded m-2 p-2 text-center"
+      class="w-1/2 md:w-1/4	lg:w-1/5 xl:w-1/6"
     >
-      <div class="text-xs px-2 h-12">
-        {{ row.title }}
-      </div>
-      <div class="text-lg font-semibold">
-        {{ row.value }}
+      <div class="m-2 p-2 border rounded text-center">
+        <div class="text-xs px-2 h-12">
+          {{ row.title }}
+        </div>
+        <div class="text-lg font-semibold">
+          {{ row.value }}
+        </div>
       </div>
     </div>
   </div>
