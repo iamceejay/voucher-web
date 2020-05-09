1a905fb886160b828e677da192dba411595059b1
Jasper Cairoane Poly <jasper.poly@engagis.com>
2020-05-09 22:59:32 +0800

    Voucher statistics styling

--- src/_pages/Vouchers/Statistics/index.vue
+++ src/_pages/Vouchers/Statistics/index.vue
@@ -2,7 +2,7 @@
   <MainLayout>
     <template #content>
       <div class="w-full h-screen flex flex-col">
-        <div class="font-bold text-lg py-2">
+        <div class="font-bold text-lg py-2 text-4xl font-display text-gray-900 capitalize">
           Voucher Statistics
         </div>
         <StatisticList 
@@ -35,28 +35,36 @@
       {
         this.stats = [
           {
-            title: 'Vouchers sold this day',
+            type: 'Vouchers',
+            title: 'sold this day',
             value: '15'
           },{
-            title: 'Earnings this day',
+            type: 'Earnings',
+            title: 'this day',
             value: '€13,456'
           },{
-            title: 'Vouchers sold this week',
+            type: 'Vouchers',
+            title: 'sold this week',
             value: '23'
           },{
-            title: 'Earnings this week',
+            type: 'Earnings',
+            title: 'this week',
             value: '€34,567'
           },{
-            title: 'Vouchers sold this month',
+            type: 'Vouchers',
+            title: 'sold this month',
             value: '123'
           },{
-            title: 'Earnings this month',
+            type: 'Earnings',
+            title: 'this month',
             value: '€123,456'
           },{
-            title: 'Vouchers sold total',
+            type: 'Vouchers',
+            title: 'sold total',
             value: '234'
           },{
-            title: 'Earnings total',
+            type: 'Earnings',
+            title: 'total',
             value: '234,456'
           },
         ]
