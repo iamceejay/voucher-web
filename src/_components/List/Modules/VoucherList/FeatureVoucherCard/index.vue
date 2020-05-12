35b3cb01f128a1f2b1655901434ccdbc06930bd5
naio.edio <karlnaio.edio@fligno.com>
2020-05-12 22:39:12 +0800

    Feature voucher: Added background color and text color

--- src/_components/List/Modules/VoucherList/FeatureVoucherCard/index.vue
+++ src/_components/List/Modules/VoucherList/FeatureVoucherCard/index.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="m-2 py-2 px-3 border border-gray-900 rounded text-center w-1/2 md:w-1/4	lg:w-1/5 xl:w-1/6">
-    <div class="px-2">
+  <div 
+    class="flex flex-col m-2 py-2 px-3 border border-gray-900 rounded bg-color text-center w-1/2 md:w-1/4	lg:w-1/5 xl:w-1/6"
+    :style="{ '--bgColor': data.bgColor }"
+    :class="`${ data.isDarkText ? 'text-black' : 'text-white' }`"
+  >
+    <div class="px-2 self-center">
       <img 
-        class="card-logo"
+        class="w-32 h-10"
         src="@/_assets/img/company-default-logo.png" 
         alt=""
       />
     </div>
-    <div class="text-sm my-2 font-bold font-display text-gray-900 h-8">
+    <div class="text-sm my-2 font-bold font-display h-8">
       {{ data.name }}
     </div>
-    <div class="text-sm font-body font-bold text-gray-900 uppercase">
+    <div class="text-sm font-body font-bold uppercase">
       €{{ data.value }}
     </div>
   </div>
@@ -36,9 +40,7 @@
   }
 </script>
 <style lang="css" scoped>
-  .card-logo {
-    width: 100%;
-    height: 40px;
-    margin-left: auto;
+  .bg-color {
+    background-color: var(--bgColor);
   }
 </style>
