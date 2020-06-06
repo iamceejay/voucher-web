622d8a5c7c0c10a8f2c8b8759976a0f29cd19403
naio.edio <karlnaio.edio@fligno.com>
2020-06-07 02:05:16 +0800

    Fixed Issues

--- src/_pages/Seller/Detail/index.vue
+++ src/_pages/Seller/Detail/index.vue
@@ -95,6 +95,7 @@
     mounted() {
       (async() => {
         await this.$store.commit('SET_IS_LOADING', { status: 'open' })
+        await this.$store.commit('SET_VOUCHERS', [])
         await this.onFetchUser()
         await this.onFetchVouchers()
         await this.$store.commit('SET_IS_LOADING', { status: 'close' })
