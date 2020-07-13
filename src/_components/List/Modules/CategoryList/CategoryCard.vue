6cd2b831a17fe051df5058811166e75cc1751bbd
naio.edio <karlnaio.edio@fligno.com>
2020-07-14 05:07:02 +0800

    Fixed category layout

--- src/_components/List/Modules/CategoryList/CategoryCard.vue
+++ src/_components/List/Modules/CategoryList/CategoryCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="m-2 py-2 px-3 shadow-custom rounded text-center card-container w-1/2 md:w-1/4	lg:w-1/5 xl:w-1/6">
     <router-link :to="`/vouchers/category/${data.id}`">
-      <div class="text-2xl my-3 font-bold font-display text-gray-900 h-8">
+      <div class="text-2xl my-3 font-bold font-display text-gray-900 h-10">
         <i :class="`fas fa-${data.icon}`" />
       </div>
       <div class="text-sm font-body font-bold text-gray-900 uppercase">
