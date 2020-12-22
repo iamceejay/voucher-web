6f3b0d7f4b0c2fd5a3436d5ba41ff6a43b08490b
Juan Miguel Torres <wanmigs@gmail.com>
2020-12-22 21:26:02 +0800

    update navbar

--- src/_components/Menus/HeaderNavMenu.vue
+++ src/_components/Menus/HeaderNavMenu.vue
@@ -1,85 +1,40 @@
 <template>
-  <div class="content-container w-full flex flex-row nav-container">
-    <div class="flex self-center nav-logo">
-      <a
-        href="javascript:void(0)"
-        :class="`flex flex-col sm:hidden self-center menu-toggle ${!hideSidebar ? 'hide' : ''}`"
-        @click="onHideSidebar()"
-      >
-        <i class="fas fa-bars text-base text-lg text-gray-900" />
-      </a>
-      <div class="flex self-center justify-center ml-12 w-full">
-        <img
-          class="self-center"
-          src="@/_assets/img/logo.png"
-          alt=""
-        />
-      </div>
+  <div id="site-header">
+    <div class="content-container w-full flex flex-row nav-container">
+      <div class="flex self-center nav-logo">
         <a
-           v-if="!AUTH_USER.isAuth"
           href="javascript:void(0)"
-          :class="`flex flex-col sm:hidden self-center ${!hideSidebar ? 'hide' : ''}`"
-          @click="showWallet = true; isRegisterPop = true"
+          :class="`flex flex-col sm:hidden self-center menu-toggle ${!hideSidebar ? 'hide' : ''}`"
+          @click="onHideSidebar()"
         >
-          <i class="fa fa-user text-lg text-gray-900" />
+          <i class="fas fa-bars text-base text-lg text-gray-900" />
         </a>
-      <!-- <span class="logo-text-1">epas</span><span class="logo-text-2">nets</span> -->
-    </div>
-    <div class="hidden sm:flex flex-row self-center nav-menu" v-if="AUTH_USER.isAuth">
-      <a
-        v-for="(menu, index) in menus"
-        :key="`menu-${index}`"
-        href="javascript:void(0)"
-        class="menu-item font-bold font-display"
-        @click="onSelectMenu(menu, index)"
-      >
-        <span class="hover:text-peach relative z-10">
-          {{ menu.title }}
-        </span>
-        <span
-          v-if="menu.child"
-          class="ml-3"
-        >
-          <i
-            :id="`dropdown-${index}`"
-            class="fas fa-caret-down text-base"
+        <div class="flex self-center justify-center ml-12 w-full">
+          <img
+            class="self-center"
+            src="@/_assets/img/logo.png"
+            alt=""
           />
-        </span>
-        <div class="fixed inset-0 z-0" v-if="menu.child && menu.isChildShow"></div>
-        <div
-          v-if="menu.child && menu.isChildShow"
-          class="dropdown-menu flex flex-col"
-        >
-          <a
-            v-for="(child, cIndex) in menu.child"
-            :key="`child-${cIndex}`"
-            href="javascript:void(0)"
-            class="dropdown-item font-bold font-display"
-            @click="onSelectMenu(child, cIndex)"
-          >
-            {{ child.title }}
-          </a>
         </div>
-      </a>
-      <a
-        v-if="AUTH_USER.isAuth"
-        href="javascript:void(0)"
-        class="menu-item font-bold font-display hover:text-peach"
-        @click="onLogout()"
-      >
-        Ausloggen
-      </a>
-    </div>
-    <div class="flex-row hidden justify-between nav-menu self-center sm:flex w-full" v-else>
-      <span>
+          <a
+            v-if="!AUTH_USER.isAuth"
+            href="javascript:void(0)"
+            :class="`flex flex-col sm:hidden self-center ${!hideSidebar ? 'hide' : ''}`"
+            @click="showWallet = true; isRegisterPop = true"
+          >
+            <i class="fa fa-user text-lg text-gray-900" />
+          </a>
+        <!-- <span class="logo-text-1">epas</span><span class="logo-text-2">nets</span> -->
+      </div>
+      <div class="hidden sm:flex flex-row self-center nav-menu" v-if="AUTH_USER.isAuth">
         <a
-          v-for="(menu, index) in menus.slice(0, 3)"
+          v-for="(menu, index) in menus"
           :key="`menu-${index}`"
           href="javascript:void(0)"
           class="menu-item font-bold font-display"
           @click="onSelectMenu(menu, index)"
         >
-          <span class="hover:text-peach">
+          <span class="hover:text-peach relative z-10">
             {{ menu.title }}
           </span>
           <span
@@ -94,7 +49,7 @@
           <div class="fixed inset-0 z-0" v-if="menu.child && menu.isChildShow"></div>
           <div
             v-if="menu.child && menu.isChildShow"
-            class="dropdown-menu flex flex-col left-0"
+            class="dropdown-menu flex flex-col"
           >
             <a
               v-for="(child, cIndex) in menu.child"
@@ -107,64 +62,111 @@
             </a>
           </div>
         </a>
-      </span>
-      <span>
         <a
-          v-for="(menu, index) in menus.slice(3)"
-          :key="`menu-${index}`"
-          :href="menu.link"
+          v-if="AUTH_USER.isAuth"
+          href="javascript:void(0)"
           class="menu-item font-bold font-display hover:text-peach"
+          @click="onLogout()"
         >
-            {{ menu.title }}
+          Ausloggen
         </a>
-      </span>
-
-    </div>
-    <router-link
-      v-if="hideSidebar && AUTH_USER && AUTH_USER.role && AUTH_USER.role.name && AUTH_USER.role.name === 'user'"
-      class="cart-icon relative self-center"
-      to="/cart"
-    >
-      <div class="cart-count" :class="`${COUNT_CART ? 'bg-peach text-white' : 'cart-count-default'}`">
-        {{ COUNT_CART }}
       </div>
-      <i class="fas fa-shopping-cart text-base text-lg" />
-    </router-link>
+      <div class="flex-row hidden justify-between nav-menu self-center sm:flex w-full" v-else>
+        <span>
+          <a
+            v-for="(menu, index) in menus.slice(0, 3)"
+            :key="`menu-${index}`"
+            href="javascript:void(0)"
+            class="menu-item font-bold font-display"
+            @click="onSelectMenu(menu, index)"
+          >
+            <span class="hover:text-peach">
+              {{ menu.title }}
+            </span>
+            <span
+              v-if="menu.child"
+              class="ml-3"
+            >
+              <i
+                :id="`dropdown-${index}`"
+                class="fas fa-caret-down text-base"
+              />
+            </span>
+            <div class="fixed inset-0 z-0" v-if="menu.child && menu.isChildShow"></div>
+            <div
+              v-if="menu.child && menu.isChildShow"
+              class="dropdown-menu flex flex-col left-0"
+            >
+              <a
+                v-for="(child, cIndex) in menu.child"
+                :key="`child-${cIndex}`"
+                href="javascript:void(0)"
+                class="dropdown-item font-bold font-display"
+                @click="onSelectMenu(child, cIndex)"
+              >
+                {{ child.title }}
+              </a>
+            </div>
+          </a>
+        </span>
+        <span>
+          <a
+            v-for="(menu, index) in menus.slice(3)"
+            :key="`menu-${index}`"
+            :href="menu.link"
+            class="menu-item font-bold font-display hover:text-peach"
+          >
+              {{ menu.title }}
+          </a>
+        </span>
 
-    <div v-if="showWallet" class="modal fixed w-full h-full top-0 left-0 flex items-center justify-center" :class="showWallet ? 'modal-active' : ''">
-      <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50" @click="showWallet = false; isRegisterPop = false"></div>
-      <div class="bg-white md:max-w-2xl modal-container mx-auto overflow-y-auto rounded shadow-lg w-11/12 z-50">
-        <div class="modal-content text-left relative">
-          <div class="absolute cursor-pointer modal-close p-4 right-0 z-50" @click="showWallet = false">
-            <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-              <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-            </svg>
-          </div>
-          <div class="p-4 sm:p-6 bg-gray-200" v-if="!isRegisterPop">
-            <div class="flex justify-between items-center pb-3">
-              <p class="font-bold text-center text-lg sm:text-2xl w-full">
-                Erhalte Zugriff zu deiner eigenen Wallet
-              </p>
+      </div>
+      <router-link
+        v-if="hideSidebar && AUTH_USER && AUTH_USER.role && AUTH_USER.role.name && AUTH_USER.role.name === 'user'"
+        class="cart-icon relative self-center"
+        to="/cart"
+      >
+        <div class="cart-count" :class="`${COUNT_CART ? 'bg-peach text-white' : 'cart-count-default'}`">
+          {{ COUNT_CART }}
+        </div>
+        <i class="fas fa-shopping-cart text-base text-lg" />
+      </router-link>
+
+      <div v-if="showWallet" class="modal fixed w-full h-full top-0 left-0 flex items-center justify-center" :class="showWallet ? 'modal-active' : ''">
+        <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50" @click="showWallet = false; isRegisterPop = false"></div>
+        <div class="bg-white md:max-w-2xl modal-container mx-auto overflow-y-auto rounded shadow-lg w-11/12 z-50">
+          <div class="modal-content text-left relative">
+            <div class="absolute cursor-pointer modal-close p-4 right-0 z-50" @click="showWallet = false">
+              <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+              </svg>
             </div>
-            <p class="text-center">In der Wallet sind deine Gutschein ein lebenlang gespeichert und du kannst von dort aus deine Gutscheine personalisieren und verschenken</p>
-          </div>
-          <div class="flex flex-col grid-template py-4 sm:grid">
-            <div class="flex flex-col p-4 sm:p-6 justify-between">
-              <p class="font-bold text-lg sm:text-2xl mx-6">Registriere dich kostenlos als Käufer</p>
-                <a href="/register/buyer" class="px-4 bg-peach p-3 rounded-full mt-8 mx-6 sm:text-lg text-white text-center">Registrieren</a>
-            </div>
-            <div class="wrapper hidden sm:block">
-              <div class="line"></div>
-              <div class="wordwrapper">
-                  <div class="word">oder</div>
+            <div class="p-4 sm:p-6 bg-gray-200" v-if="!isRegisterPop">
+              <div class="flex justify-between items-center pb-3">
+                <p class="font-bold text-center text-lg sm:text-2xl w-full">
+                  Erhalte Zugriff zu deiner eigenen Wallet
+                </p>
               </div>
+              <p class="text-center">In der Wallet sind deine Gutschein ein lebenlang gespeichert und du kannst von dort aus deine Gutscheine personalisieren und verschenken</p>
             </div>
-            <div id="oder" class="block sm:hidden mt-10">
-              <span class="bg-white px-3">oder</span>
-            </div>​
-            <div class="flex flex-col p-6 justify-between">
-              <p class="font-bold text-lg sm:text-2xl mx-6">Melde dich als Käufer an</p>
-                <a href="/login" class="px-4 bg-peach p-3 rounded-full mt-8 mx-6 sm:text-lg text-white text-center">Anmelden</a>
+            <div class="flex flex-col grid-template py-4 sm:grid">
+              <div class="flex flex-col p-4 sm:p-6 justify-between">
+                <p class="font-bold text-lg sm:text-2xl mx-6">Registriere dich kostenlos als Käufer</p>
+                  <a href="/register/buyer" class="px-4 bg-peach p-3 rounded-full mt-8 mx-6 sm:text-lg text-white text-center">Registrieren</a>
+              </div>
+              <div class="wrapper hidden sm:block">
+                <div class="line"></div>
+                <div class="wordwrapper">
+                    <div class="word">oder</div>
+                </div>
+              </div>
+              <div id="oder" class="block sm:hidden mt-10">
+                <span class="bg-white px-3">oder</span>
+              </div>​
+              <div class="flex flex-col p-6 justify-between">
+                <p class="font-bold text-lg sm:text-2xl mx-6">Melde dich als Käufer an</p>
+                  <a href="/login" class="px-4 bg-peach p-3 rounded-full mt-8 mx-6 sm:text-lg text-white text-center">Anmelden</a>
+              </div>
             </div>
           </div>
         </div>
@@ -209,6 +211,58 @@
     },
     mounted() {
       (async() => {
+        (function(){
+          var doc = document.documentElement;
+          var w = window;
+
+          var prevScroll = w.scrollY || doc.scrollTop;
+          var curScroll;
+          var direction = 0;
+          var prevDirection = 0;
+
+          var header = document.getElementById('site-header');
+
+          var checkScroll = function() {
+
+            /*
+            ** Find the direction of scroll
+            ** 0 - initial, 1 - up, 2 - down
+            */
+
+            curScroll = w.scrollY || doc.scrollTop;
+            if (curScroll > prevScroll) {
+              //scrolled up
+              direction = 2;
+            }
+            else if (curScroll < prevScroll) {
+              //scrolled down
+              direction = 1;
+            }
+
+            if (direction !== prevDirection) {
+              toggleHeader(direction, curScroll);
+            }
+
+            prevScroll = curScroll;
+          };
+
+          var toggleHeader = function(direction, curScroll) {
+            if (direction === 2 && curScroll > 52) {
+
+              //replace 52 with the height of your header in px
+
+              header.classList.add('hide');
+              prevDirection = direction;
+            }
+            else if (direction === 1) {
+              header.classList.remove('hide');
+              prevDirection = direction;
+            }
+          };
+
+          window.addEventListener('scroll', checkScroll);
+
+        })();
         await this.onFetchData()
         this.$emit('onHide', this.hideSidebar)
         this.$nextTick(function() {
@@ -465,6 +519,21 @@
   .nav-container {
     /* border: 1px solid #ccc; */
     height: 80px;
+  }
+  #site-header {
+    position: fixed;
+    height: 80px;
+    background: #fff;
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    z-index: 100;
+    transition: all .3s ease;
+    box-shadow: 0 1px 25px rgba(0,0,0, .1);
+  }
+  #site-header.hide {
+    top: -81px;
   }
   .nav-container .nav-logo {
     padding: 0px 20px;
