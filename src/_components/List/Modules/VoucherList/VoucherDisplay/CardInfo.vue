280431a4f8167b2e9a17ed05dcf0572e2ee0c199
Juan Miguel Torres <wanmigs@gmail.com>
2021-03-29 15:41:06 +0800

    fix redemtion

--- src/_components/List/Modules/VoucherList/VoucherDisplay/CardInfo.vue
+++ src/_components/List/Modules/VoucherList/VoucherDisplay/CardInfo.vue
@@ -65,18 +65,18 @@
           <span class="text-xl font-bold" v-if="userVoucher">
             <span v-show="!voucher.price_hidden || typeof voucher.price_hidden == 'undefined'">{{
                 `${(voucher.type == 'quantity')
-                  ? `${$helpers.convertCurrency( $route.name == 'wallet' ? order.total_amount : voucher.qty_val)}`
-                  : $route.name == 'wallet'
-                  ? $helpers.convertCurrency(order.total_amount)
+                  ? `${$helpers.convertCurrency( ['wallet', 'voucher-detail'].indexOf($route.name) != -1 ? order.total_amount : voucher.qty_val)}`
+                  : ['wallet', 'voucher-detail'].indexOf($route.name) != -1
+                  ? $helpers.convertCurrency(order.value)
                   : `${$helpers.convertCurrency(voucher.min || voucher.val_min).replace('€', '')} - ${$helpers.convertCurrency(voucher.max || voucher.val_max)}`}`
             }}</span>
           </span>
           <span class="text-xl font-bold" v-else>
               {{
                 `${(voucher.type == 'quantity')
-                  ? `${$helpers.convertCurrency($route.name == 'wallet' ? order.total_amount : voucher.qty_val)}`
-                  : $route.name == 'wallet'
-                  ? $helpers.convertCurrency(order.total_amount)
+                  ? `${$helpers.convertCurrency(['wallet', 'voucher-detail'].indexOf($route.name) != -1 ? order.total_amount : voucher.qty_val)}`
+                  : ['wallet', 'voucher-detail'].indexOf($route.name) != -1
+                  ? $helpers.convertCurrency(order.value)
                   :`${$helpers.convertCurrency(voucher.min || voucher.val_min).replace('€', '')} ${  ['vouchers-detail', 'cart'].indexOf($route.name) != -1 ? '- ' + $helpers.convertCurrency(voucher.max || voucher.val_max) : ''}`}`
               }}
               <span></span>
