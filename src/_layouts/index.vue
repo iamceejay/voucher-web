be5693cdf0021c9d10186264e316082368aad909
Juan Miguel Torres <wanmigs@gmail.com>
2021-04-16 15:30:28 +0800

    hide header on scroll in mobile

--- src/_layouts/index.vue
+++ src/_layouts/index.vue
@@ -1,20 +1,6 @@
 <template>
-  <div
-    id="main-layout-component"
-    class="absolute hide-sidebar inset-0 grid h-full"
-    :class="{
-      'md:block': !AUTH_USER.isAuth || (AUTH_USER.isAuth && AUTH_USER.role.name == 'user'),
-      'overflow-hidden': !AUTH_USER.isAuth && categories.length,
-    }"
-  >
-    <div v-if="IS_LOADING.data || IS_PROCESSING.data" class="absolute bg-black bg-opacity-25 inset-0" style="z-index: 99999;">
-      <lottie-animation path="./loading.json"/>
-    </div>
-    <HeaderNavMenu
-      ref="header"
-      @onShowSubMenu="handleCategory"
-    />
-    <div class="bg-white md:hidden p-4 flex items-center justify-between border-b">
+  <section>
+    <div id="mobile-header" class="bg-white md:hidden p-4 flex items-center justify-between border-b">
       <router-link
         to="/home"
         >
@@ -84,67 +70,84 @@
 
     </div>
     <div
-      class="flex flex-col w-full relative"
-      :class="`${categories.length ? 'overflow-hidden' : 'overflow-auto overflow-x-hidden'} ${!AUTH_USER.isAuth || AUTH_USER.role.name === 'user' ? 'guest-container' : ''}`"
-      :style="contentStyle"
+      id="main-layout-component"
+      class="absolute hide-sidebar inset-0 grid h-full"
+      :class="{
+        'md:block': !AUTH_USER.isAuth || (AUTH_USER.isAuth && AUTH_USER.role.name == 'user'),
+        'overflow-hidden': !AUTH_USER.isAuth && categories.length,
+      }"
     >
-      <ul
-        v-if="categories.length"
-        id="mobile-category"
-        class="categories-menu fixed inset-0 z-110 px-6 py-4 overflow-auto "
-        :class="AUTH_USER.isAuth && AUTH_USER.role.name !== 'user' ? 'md:hidden' : 'guest'"
-        style="background-color: #F2F2F2">
-          <li v-for="(category, index) in categories" :key="index" class="categories-menu__item content-container">
-            <a :href="category.link" class="flex flex-wrap items-center py-2 lg:py-4 text-sm">
-              <img  v-if="category.icon.indexOf('fas') == -1" :src="onSetLogo('set', category.icon)" class="h-12 lg:h-16 rounded-md mr-6 lg:mr-8" :alt="category.title">
-              <img v-else class="h-12 lg:h-16 rounded-md mr-6 lg:mr-8" src="@/_assets/img/placeholder-400-300.jpg" :alt="category.title">
-              <span>{{ category.title }}</span>
-            </a>
-          </li>
-      </ul>
-      <div class="w-full">
-        <div
-          id="infinite-scroll"
-          class="flex flex-col h-full w-full m-c pt-10"
-        >
-          <BackBtn class="px-8" :title="title" v-if="showBackButton" />
-          <slot name="content" />
-        </div>
+      <div v-if="IS_LOADING.data || IS_PROCESSING.data" class="absolute bg-black bg-opacity-25 inset-0" style="z-index: 99999;">
+        <lottie-animation path="./loading.json"/>
       </div>
-
-      <Footer v-if="!AUTH_USER.isAuth || (AUTH_USER.role.name === 'user' && $route.name == 'vouchers-search')"/>
-    </div>
-      <Sidebar @onShowSubMenu="handleCategory"
+      <HeaderNavMenu
+        ref="header"
+        @onShowSubMenu="handleCategory"
       />
-    <CookieLaw
-      class="cookie-container"
-      buttonClass="cookie-btn"
-    >
-      <template #default="props">
-        <div class="flex flex-col">
-          <p class="text-xs">
-            Diese Webseite verwendet Cookies, um dir das bestmögliche Erlebnis zu bieten.
-          </p>
-          <p class="text-xs">
-            <a
-              class="text-peach font-bold"
-              href="https://verkaufen.epasnets.com/datenschutz"
-              target="_blank"
-            >
-              Klicke hier,
-            </a>
-            um mehr über die Datenschutzbedingungen zu erfahren.
-          </p>
+
+      <div
+        class="flex flex-col w-full relative"
+        :class="`${categories.length ? 'overflow-hidden' : 'overflow-auto overflow-x-hidden'} ${!AUTH_USER.isAuth || AUTH_USER.role.name === 'user' ? 'guest-container' : ''}`"
+        :style="contentStyle"
+      >
+        <ul
+          v-if="categories.length"
+          id="mobile-category"
+          class="categories-menu fixed inset-0 z-110 px-6 py-4 overflow-auto "
+          :class="AUTH_USER.isAuth && AUTH_USER.role.name !== 'user' ? 'md:hidden' : 'guest'"
+          style="background-color: #F2F2F2">
+            <li v-for="(category, index) in categories" :key="index" class="categories-menu__item content-container">
+              <a :href="category.link" class="flex flex-wrap items-center py-2 lg:py-4 text-sm">
+                <img  v-if="category.icon.indexOf('fas') == -1" :src="onSetLogo('set', category.icon)" class="h-12 lg:h-16 rounded-md mr-6 lg:mr-8" :alt="category.title">
+                <img v-else class="h-12 lg:h-16 rounded-md mr-6 lg:mr-8" src="@/_assets/img/placeholder-400-300.jpg" :alt="category.title">
+                <span>{{ category.title }}</span>
+              </a>
+            </li>
+        </ul>
+        <div class="w-full">
+          <div
+            id="infinite-scroll"
+            class="flex flex-col h-full w-full m-c pt-10"
+          >
+            <BackBtn class="px-8" :title="title" v-if="showBackButton" />
+            <slot name="content" />
+          </div>
         </div>
-        <button
-          class="cookie-btn"
-          @click="props.accept"
-        >
-          Akzeptieren
-        </button>
-      </template>
-    </CookieLaw>
-  </div>
+
+        <Footer v-if="!AUTH_USER.isAuth || (AUTH_USER.role.name === 'user' && $route.name == 'vouchers-search')"/>
+      </div>
+        <Sidebar @onShowSubMenu="handleCategory"
+        />
+      <CookieLaw
+        class="cookie-container"
+        buttonClass="cookie-btn"
+      >
+        <template #default="props">
+          <div class="flex flex-col">
+            <p class="text-xs">
+              Diese Webseite verwendet Cookies, um dir das bestmögliche Erlebnis zu bieten.
+            </p>
+            <p class="text-xs">
+              <a
+                class="text-peach font-bold"
+                href="https://verkaufen.epasnets.com/datenschutz"
+                target="_blank"
+              >
+                Klicke hier,
+              </a>
+              um mehr über die Datenschutzbedingungen zu erfahren.
+            </p>
+          </div>
+          <button
+            class="cookie-btn"
+            @click="props.accept"
+          >
+            Akzeptieren
+          </button>
+        </template>
+      </CookieLaw>
+    </div>
+  </section>
 </template>
 <script>
   import Sidebar from '_components/Menus/Sidebar'
@@ -296,7 +299,56 @@
     },
     mounted() {
       (async() => {
-        this.onScroll()
+        var doc = document.querySelectorAll('.guest-container')[0];
+
+        var prevScroll = doc.scrollTop;
+        var curScroll;
+        var direction = 0;
+        var prevDirection = 0;
+
+        var mobile = document.getElementById('mobile-header');
+
+        var checkScroll = function() {
+
+          /*
+          ** Find the direction of scroll
+          ** 0 - initial, 1 - up, 2 - down
+          */
+
+          curScroll = doc.scrollTop;
+          if (curScroll > prevScroll) {
+            //scrolled up
+            direction = 2;
+          }
+          else if (curScroll < prevScroll) {
+            //scrolled down
+            direction = 1;
+          }
+
+          if (direction !== prevDirection) {
+            toggleHeader(direction, curScroll);
+          }
+
+          prevScroll = curScroll;
+        };
+
+        var toggleHeader = function(direction, curScroll) {
+          if (direction === 2 && curScroll > 52) {
+
+            //replace 52 with the height of your header in px
+
+            mobile.classList.add('hide');
+            prevDirection = direction;
+          }
+          else if (direction === 1) {
+            mobile.classList.remove('hide');
+            prevDirection = direction;
+          }
+        };
+
+        doc.addEventListener('scroll', checkScroll);
+
+        this.onScroll();
       })()
     },
     methods: {
@@ -371,12 +423,17 @@
   }
   #main-layout-component {
     margin: 0 auto;
-    grid-template-rows: 70px 1fr 70px;
+    grid-template-rows: 1fr 70px;
   }
 
   .main-container.hide {
     transition: all 0.6s;
   }
+
+  .guest-container {
+    padding-top: 70px;
+  }
+
   @media only screen and (max-width: 600px) {
     .main-container.hide {
       background: rgba(0, 0, 0, 0.80);
@@ -405,5 +462,19 @@
     .guest-container {
       margin-top: 90px;
     }
+  }
+
+  #mobile-header {
+    height: 70px;
+    position: fixed;
+    top: 0;
+    z-index: 9999;
+    transition: top .3s ease;
+    left: 0;
+    right: 0;
+  }
+
+  #mobile-header.hide {
+      top: -71px;
   }
 </style>
