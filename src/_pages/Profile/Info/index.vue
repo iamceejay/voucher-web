9490f3b69f732d939fe8a9a2b55b1b947d22a718
Jasper Cairoane Poly <jaspercairoane.poly@gmail.com>
2021-04-23 10:05:25 -0700

    Fix years and months not showing when changing birthdate

--- src/_pages/Profile/Info/index.vue
+++ src/_pages/Profile/Info/index.vue
@@ -18,28 +18,31 @@ export default {
       return this.$store.getters.AUTH_USER.role.name == 'seller' ? 'Seller' : 'Buyer'
     }
   },
+  beforeMount() {
+    document.body.classList.remove('voucher-builder-page')
+  },
   beforeRouteLeave(to, from, next) {
-      if (this.$refs.form.isSubmitted || !this.$refs.form.isFormDirty) {
-        return next(true)
-      }
+    if (this.$refs.form.isSubmitted || !this.$refs.form.isFormDirty) {
+      return next(true)
+    }
 
-      this.$swal({
-        title: 'Möchten Sie Änderungen speichern?',
-        text: '',
-        showCancelButton: true,
-        confirmButtonColor: '#48BB78',
-        cancelButtonColor: '#FC8181',
-        confirmButtonText: 'speichern',
-        cancelButtonText: 'Verwerfen',
-      }).then(async (result) => {
-        if(result.value) {
-          this.$refs.form.isRouteChanged = true
-          let response = await this.$refs.form.onSubmit()
-          next(response)
-        } else {
-          next(true)
-        }
-      })
-    },
+    this.$swal({
+      title: 'Möchten Sie Änderungen speichern?',
+      text: '',
+      showCancelButton: true,
+      confirmButtonColor: '#48BB78',
+      cancelButtonColor: '#FC8181',
+      confirmButtonText: 'speichern',
+      cancelButtonText: 'Verwerfen',
+    }).then(async (result) => {
+      if(result.value) {
+        this.$refs.form.isRouteChanged = true
+        let response = await this.$refs.form.onSubmit()
+        next(response)
+      } else {
+        next(true)
+      }
+    })
+  },
 }
 </script>
