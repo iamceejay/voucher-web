8374351e2d09f89a46897f3dc7054310f000f805
Juan Miguel Torres <wanmigs@gmail.com>
2021-04-30 16:45:45 +0800

    update

--- src/_components/Form/Modules/PaymentForm.vue
+++ src/_components/Form/Modules/PaymentForm.vue
@@ -335,7 +335,7 @@
                 // instead check for: `paymentIntent.status === "requires_source_action"`.
                 if (confirmResult.paymentIntent.status === "requires_action") {
                   // Let Stripe.js handle the rest of the payment flow.
-                  this.stripe.confirmCardPayment(data.client_secret).then( async function(result) {
+                  this.stripe.confirmCardPayment(data.client_secret).then( async (result) => {
                     if (result.error) {
                       // The payment failed -- ask your customer for a new payment method.
                     } else {
