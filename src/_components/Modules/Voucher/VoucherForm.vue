7754fbded77d2a1c6ed9348de60e2683cf51b12c
Juan Miguel Torres <wanmigs@gmail.com>
2021-05-04 14:46:08 +0800

    add button to switch tab on voucher builder

--- src/_components/Modules/Voucher/VoucherForm.vue
+++ src/_components/Modules/Voucher/VoucherForm.vue
@@ -12,6 +12,18 @@
             :voucher="form"
             :isFlippable="false"
           />
+          <div class="flex max-w-xs mt-4 mx-auto space-x-3 w-full">
+            <button type="button" @click="prev()" class="bg-peach px-5 py-3 rounded-md text-sm text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+              </svg>
+            </button>
+            <button type="button" @click="next()" class="bg-peach px-5 py-3 rounded-md text-sm text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+              </svg>
+            </button>
+          </div>
         </div>
 
         <div class="w-full flex flex-col" style="max-width: 368px">
@@ -1127,6 +1139,18 @@
         e.target.parentElement.classList.add('hidden')
         this.form[form] = ''
         this.form[form + '_update'] = false
+      },
+      next() {
+        this.currentTab++
+        if ( this.currentTab > 2) {
+          this.currentTab = 0
+        }
+      },
+      prev() {
+        this.currentTab--
+        if (this.currentTab < 0) {
+          this.currentTab = 2
+        }
       }
     }
   }
