4e8fdcf8dda46d5327234f356584c820d2aaf376
Juan Miguel Torres <wanmigs@gmail.com>
2021-05-14 15:15:38 +0800

    full month to german

--- src/_pages/Vouchers/Detail/index.vue
+++ src/_pages/Vouchers/Detail/index.vue
@@ -197,7 +197,7 @@
                   </div>
                 </div>
                 <span v-else class="text-xs flex flex-col">
-                  Jan<br/> Feb<br/> Mär<br/> Apr<br/> Mai<br/> Jun<br/> Jul<br/> Aug<br/> Sep<br/> Okt<br/> Nov<br/> Dez
+                  Januar<br/> Februar<br/> März<br/> April<br/> Mai<br/> Juni<br/> Juli<br/> August<br/> September<br/> Oktober<br/> November<br/> Dezember
                 </span>
               </div>
 
