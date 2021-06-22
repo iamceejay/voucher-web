59e3c6a8b3f98c978ea86fefe05118634695964e
Jasper Cairoane Poly <jaspercairoane.poly@gmail.com>
2021-06-22 00:40:13 -0700

    Fix seller registration fields

--- src/_components/Modules/Profile/Form/VerificationForm.vue
+++ src/_components/Modules/Profile/Form/VerificationForm.vue
@@ -33,7 +33,7 @@
         @input="onChange"
       />
       <span class="block font-semibold text-sm mb-4">Um die ersten Auszahlungen zu erhalten musst du dich mittels den genannten Dokumenten verifizieren.</span>
-               <span class="block text-sm mb-4">(Personalausweis, Führerschein oder Pass. Und ein amtliches Dokument mit der Adresse des Unternehmens zb. Firmenbuchauszug oder Anmeldung. Bitte lade eine JPEG oder PNG Datei hoch (keine PDF). Foto sollte im Hochformat und gut lesbar sein.)</span>
+      <span class="block text-sm mb-4">(Personalausweis, Führerschein oder Pass. Und ein amtliches Dokument mit der Adresse des Unternehmens zb. Firmenbuchauszug oder Anmeldung. Bitte lade eine JPEG oder PNG Datei hoch (keine PDF). Foto sollte im Hochformat und gut lesbar sein.)</span>
       <div class="gap-4 md:grid grid-cols-3">
         <div class="mb-4">
           <div
@@ -45,17 +45,6 @@
               :src="onSetImage('set', verification_front)"
               alt=""
             />
-          </div>
-          <div
-            v-if="!verification_front"
-            class="flex mx-2 w-full"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-check mx-auto text-green-500" style="width: 150px; height: 150px;" viewBox="0 0 16 16">
-              <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
-              <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
-            </svg>
-
-            <div class="my-2 text-center">Dokument hochgeladen</div>
           </div>
           <SellerUploadFile
             v-if="type == 'seller'"
@@ -83,17 +72,6 @@
               alt=""
             />
           </div>
-          <div
-            v-if="!verification_back"
-            class="flex mx-2 w-full"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-check mx-auto text-green-500" style="width: 150px; height: 150px;" viewBox="0 0 16 16">
-              <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
-              <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
-            </svg>
-
-            <div class="my-2 text-center">Dokument hochgeladen</div>
-          </div>
           <SellerUploadFile
             v-if="type == 'seller'"
             id="icon"
@@ -119,17 +97,6 @@
               :src="onSetImage('set', additional_identity)"
               alt=""
             />
-          </div>
-          <div
-            v-if="!additional_identity"
-            class="flex mx-2 w-full"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-check mx-auto text-green-500" style="width: 150px; height: 150px;" viewBox="0 0 16 16">
-              <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
-              <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
-            </svg>
-
-            <div class="my-2 text-center">Dokument hochgeladen</div>
           </div>
           <SellerUploadFile
             v-if="type == 'seller'"
